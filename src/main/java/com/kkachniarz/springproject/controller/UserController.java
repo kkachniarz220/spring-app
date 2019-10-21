bd982f3aa280f35ba35b9d05f440c45a9bf7afc9
Krzysztof Kachniarz <krzysztof.kachniarz@fakro.pl>
2019-10-21 13:50:55 +0200

    Full endpoint

--- src/main/java/com/kkachniarz/springproject/controller/UserController.java
+++ src/main/java/com/kkachniarz/springproject/controller/UserController.java
@@ -5,43 +5,64 @@ import com.kkachniarz.springproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = {"http://localhost:4200" , "http://172.17.0.43:4200"})
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
+
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping(path = "/add")
-    public String addNewUser(@RequestParam String name, @RequestParam String email) {
-        User user = new User();
-        user.setName(name);
-        user.setEmail(email);
+    public ResponseEntity addNewUser(@RequestBody User user) {
         userRepository.save(user);
-        return "OK";
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/list")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @GetMapping(path="/{id}")
-    public ResponseEntity getUser(@PathVariable() Integer id) {
-        System.out.println(id);
+    @GetMapping(path = "/{userId}")
+    public ResponseEntity getUser(@PathVariable() Integer userId) {
         try {
-            Optional<User> foundUser =  userRepository.findById(id);
+            Optional<User> foundUser = userRepository.findById(userId);
             System.out.println(foundUser);
             return ResponseEntity.of(foundUser);
         } catch (Exception e) {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
+    }
+
+    @PostMapping(path = "/edit/{userId}")
+    public ResponseEntity editUser(@PathVariable() Integer userId, @RequestBody User user) {
+        try {
+            Optional<User> foundUser = userRepository.findById(userId);
+            foundUser.get().setName(user.getName());
+            foundUser.get().setEmail(user.getEmail());
+            userRepository.save(foundUser.get());
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(path = "/delete")
+    public ResponseEntity deleteUser(@RequestParam("userId") Integer userId) {
+        try {
+            userRepository.deleteById(userId);
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
     }
 }
